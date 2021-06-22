d13099969c5aee76ac64de6416d7cc0abc9ddd6c
Gang Li <ligangty@users.noreply.github.com>
2021-06-22 18:03:01 +0800

    Support concurrent download/upload

--- buildtest/run.go
+++ buildtest/run.go
@@ -9,6 +9,7 @@ import (
 	"path"
 	"regexp"
 	"strings"
+	"sync"
 	"time"
 )
 
@@ -46,30 +47,36 @@ func Run(logUrl, replacement, targetIndy, buildType string, processNum int) {
 	if err == nil {
 		downloads := replaceTargets(decorateChecksums(result["downloads"]), "", indyHost, newBuildName)
 		result["downloads"] = nil // save memory
+		downloadFunc := func(artifactUrl string) {
+			fileLoc := path.Join(TMP_DOWNLOAD_DIR, path.Base(artifactUrl))
+			DownloadFile(artifactUrl, fileLoc)
+		}
 		if downloads != nil {
 			if processNum > 1 {
-				//TODO: implement concurrent download here with processNum
+				concurrentRun(processNum, downloads, downloadFunc)
 			} else {
 				for _, url := range downloads {
-					fileLoc := path.Join(TMP_DOWNLOAD_DIR, path.Base(url))
-					DownloadFile(url, fileLoc)
+					downloadFunc(url)
 				}
 			}
 		}
 		// uploads := replaceTargets(result["uploads"], "", indyHost, newBuildName)
 		// result["uploads"] = nil // save memory
+		uploadFunc := func(artifactUrl string) {
+			cacheFile := path.Join(TMP_UPLOAD_DIR, path.Base(artifactUrl))
+			downloadArtifact := replaceHost(artifactUrl, "", indyHost)
+			downloaded := DownloadUploadFileForCache(downloadArtifact, cacheFile)
+			if downloaded {
+				replacedUrl := replaceBuildName(downloadArtifact, newBuildName)
+				UploadFile(replacedUrl, cacheFile)
+			}
+		}
 		if result["uploads"] != nil {
 			if processNum > 1 {
-				//TODO: implement concurrent upload here with processNum
+				concurrentRun(processNum, result["uploads"], uploadFunc)
 			} else {
 				for _, url := range result["uploads"] {
-					cacheFile := path.Join(TMP_UPLOAD_DIR, path.Base(url))
-					downloadArtifact := replaceHost(url, "", indyHost)
-					downloaded := DownloadUploadFileForCache(downloadArtifact, cacheFile)
-					if downloaded {
-						replacedUrl := replaceBuildName(downloadArtifact, newBuildName)
-						UploadFile(replacedUrl, cacheFile)
-					}
+					uploadFunc(url)
 				}
 			}
 		}
@@ -212,4 +219,32 @@ func generateRandomBuildName() string {
 	min := 10000
 	max := 99999
 	return fmt.Sprintf(buildPrefix+"%v", rand.Intn(max-min)+min)
+}
+
+func concurrentRun(numWorkers int, artifacts []string, job func(artifact string)) {
+	var ch = make(chan string, numWorkers*5) // This buffered number of chan can be anything as long as it's larger than numWorkers
+	var wg sync.WaitGroup
+
+	// This starts xthreads number of goroutines that wait for something to do
+	wg.Add(numWorkers)
+	for i := 0; i < numWorkers; i++ {
+		go func() {
+			for {
+				a, ok := <-ch
+				if !ok { // if there is nothing to do and the channel has been closed then end the goroutine
+					wg.Done()
+					return
+				}
+				job(a) // do the thing
+			}
+		}()
+	}
+
+	// Now the jobs can be added to the channel, which is used as a queue
+	for _, artifact := range artifacts {
+		ch <- artifact // add i to the queue
+	}
+
+	close(ch) // This tells the goroutines there's nothing else to do
+	wg.Wait() // Wait for the threads to finish
 }
