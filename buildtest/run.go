d63e22b018e0ff22a6a3fc772e41ed3b7fea88c5
Gang Li <ligangty@users.noreply.github.com>
2021-07-27 00:01:12 +0800

    Fix a bug of upload url construction

--- buildtest/run.go
+++ buildtest/run.go
@@ -36,23 +36,27 @@ func Run(originalIndy, foloId, replacement, targetIndy, buildType string, proces
 
 	// result := prepareEntriesByLog(logUrl)
 
-	result := prepareEntriesByFolo(originalIndy, targetIndy, foloId, newBuildName)
+	// result := prepareEntriesByFolo(originalIndy, targetIndy, foloId, newBuildName)
+
+	origIndy := originalIndy
+	if !strings.HasPrefix(origIndy, "http://") {
+		origIndy = "http://" + origIndy
+	}
+	foloTrackContent := common.GetFoloRecord(origIndy, foloId)
 
 	prepareCacheDirectories()
 
-	broken := false
-	fmt.Println("Start handling downloads artifacts.")
-	fmt.Printf("==========================================\n\n")
-	downloads := result["downloads"]
-	// downloads := replaceTargets(result["downloads"], "", targetIndyHost, newBuildName)
-	result["downloads"] = nil // save memory
+	downloads := prepareDownloadEntriesByFolo(targetIndy, newBuildName, foloTrackContent)
 	downloadFunc := func(artifactUrl string) bool {
 		fileLoc := path.Join(TMP_DOWNLOAD_DIR, path.Base(artifactUrl))
 		return common.DownloadFile(artifactUrl, fileLoc)
 	}
-	if downloads != nil {
+	broken := false
+	if downloads != nil && len(downloads) > 0 {
+		fmt.Println("Start handling downloads artifacts.")
+		fmt.Printf("==========================================\n\n")
 		if processNum > 1 {
-			broken = !concurrentRun(processNum, downloads, downloadFunc)
+			broken = !concurrentRunForDownload(processNum, downloads, downloadFunc)
 		} else {
 			for _, url := range downloads {
 				broken = !downloadFunc(url)
@@ -61,91 +65,90 @@ func Run(originalIndy, foloId, replacement, targetIndy, buildType string, proces
 				}
 			}
 		}
-	}
-	fmt.Println("==========================================")
-	if broken {
-		fmt.Printf("Build test failed due to some downloading errors. Please see above logs to see the details.\n\n")
-		os.Exit(1)
+		fmt.Println("==========================================")
+		if broken {
+			fmt.Printf("Build test failed due to some downloading errors. Please see above logs to see the details.\n\n")
+			os.Exit(1)
+		}
+		fmt.Printf("Downloads artifacts handling finished.\n\n")
 	}
 
-	fmt.Printf("Downloads artifacts handling finished.\n\n")
-
-	fmt.Println("Start handling uploads artifacts.")
-	fmt.Printf("==========================================\n\n")
-	// uploads := replaceTargets(result["uploads"], "", indyHost, newBuildName)
-	// result["uploads"] = nil // save memory
-	uploadFunc := func(artifactUrl string) bool {
-		cacheFile := path.Join(TMP_UPLOAD_DIR, path.Base(artifactUrl))
-		downloaded := common.DownloadUploadFileForCache(artifactUrl, cacheFile)
+	uploadFunc := func(originalArtiURL, targetArtiURL string) bool {
+		cacheFile := path.Join(TMP_UPLOAD_DIR, path.Base(originalArtiURL))
+		downloaded := common.DownloadUploadFileForCache(originalArtiURL, cacheFile)
 		if downloaded {
-			// after download from original indy, need to replace indy host in path to target indy
-			// host to do the final uploading
-			replacedUrl := replaceHost(artifactUrl, "", targetIndyHost)
-			replacedUrl = replaceBuildName(replacedUrl, newBuildName)
-			return common.UploadFile(replacedUrl, cacheFile)
+			return common.UploadFile(targetArtiURL, cacheFile)
 		}
 		return false
 	}
-	if result["uploads"] != nil {
+
+	uploads := prepareUploadEntriesByFolo(originalIndy, targetIndy, newBuildName, foloTrackContent)
+
+	if uploads != nil && len(uploads) > 0 {
+		fmt.Println("Start handling uploads artifacts.")
+		fmt.Printf("==========================================\n\n")
 		if processNum > 1 {
-			broken = !concurrentRun(processNum, result["uploads"], uploadFunc)
+			broken = !concurrentRunForUpload(processNum, uploads, uploadFunc)
 		} else {
-			for _, url := range result["uploads"] {
-				broken = !uploadFunc(url)
+			for _, artiURLs := range uploads {
+				broken = !uploadFunc(artiURLs[0], artiURLs[1])
 				if broken {
 					break
 				}
 			}
 		}
-	}
-	fmt.Println("==========================================")
-	if broken {
-		fmt.Printf("Build test failed due to some uploadig errors. Please see above logs to see the details.\n\n")
-		os.Exit(1)
-	}
+		fmt.Println("==========================================")
+		if broken {
+			fmt.Printf("Build test failed due to some uploadig errors. Please see above logs to see the details.\n\n")
+			os.Exit(1)
+		}
 
-	fmt.Printf("Uploads artifacts handling finished.\n\n")
+		fmt.Printf("Uploads artifacts handling finished.\n\n")
+	}
 }
 
-func prepareEntriesByFolo(originalIndyURL, targetIndyURL, foloId, newBuildId string) map[string][]string {
-	originalIndy := originalIndyURL
-	if !strings.HasPrefix(originalIndy, "http://") {
-		originalIndy = "http://" + originalIndy
-	}
-	foloTrackContent := common.GetFoloRecord(originalIndy, foloId)
-
-	result := make(map[string][]string)
-
-	// For downloads entries, we will get the paths and inject them to the final url of target indy
-	// as they should be directly download from target indy.
+// For downloads entries, we will get the paths and inject them to the final url of target indy
+// as they should be directly download from target indy.
+func prepareDownloadEntriesByFolo(targetIndyURL, newBuildId string, foloRecord common.TrackedContent) []string {
 	downloads := []string{}
-	targetIndy := targetIndyURL
-	if !strings.HasPrefix(targetIndy, "http://") {
-		targetIndy = "http://" + targetIndy
-	}
-	if !strings.HasSuffix(targetIndy, "/") {
-		targetIndy = targetIndy + "/"
-	}
-	for _, down := range foloTrackContent.Downloads {
+	targetIndy := normIndyURL(targetIndyURL)
+	for _, down := range foloRecord.Downloads {
 		downUrl := fmt.Sprintf("%sapi/folo/track/%s/maven/group/%s%s", targetIndy, newBuildId, newBuildId, down.Path)
 		downloads = append(downloads, downUrl)
 	}
-	result["downloads"] = downloads
+	return downloads
+}
 
-	// For uploads entries, firstly they should be downloaded from original indy server as they may not
-	// exist in target indy server, so need to use original indy server to make the path
-	uploads := []string{}
-	if !strings.HasSuffix(originalIndy, "/") {
-		originalIndy = originalIndy + "/"
-	}
-	for _, up := range foloTrackContent.Uploads {
+// For uploads entries, firstly they should be downloaded from original indy server as they may not
+// exist in target indy server, so need to use original indy server to make the download url, and
+// use the target indy server to make the upload url
+func prepareUploadEntriesByFolo(originalIndyURL, targetIndyURL, newBuildId string, foloRecord common.TrackedContent) map[string][]string {
+	originalIndy := normIndyURL(originalIndyURL)
+	targetIndy := normIndyURL(targetIndyURL)
+	result := make(map[string][]string)
+	for _, up := range foloRecord.Uploads {
+		upTuple := []string{}
 		storePath := common.StoreKeyToPath(up.StoreKey)
 		uploadPath := path.Join("api/content", storePath, up.Path)
-		upUrl := fmt.Sprintf("%s%s", originalIndy, uploadPath)
-		uploads = append(uploads, upUrl)
+		orgiUpUrl := fmt.Sprintf("%s%s", originalIndy, uploadPath)
+		upTuple = append(upTuple, orgiUpUrl)
+		targUpUrl := fmt.Sprintf("%sapi/folo/track/%s/maven/group/%s%s", targetIndy, newBuildId, newBuildId, up.Path)
+		upTuple = append(upTuple, targUpUrl)
+		result[up.Path] = upTuple
 	}
-	result["uploads"] = uploads
+
 	return result
+}
+
+func normIndyURL(indyURL string) string {
+	indy := indyURL
+	if !strings.HasPrefix(indy, "http://") {
+		indy = "http://" + indy
+	}
+	if !strings.HasSuffix(indy, "/") {
+		indy = indy + "/"
+	}
+	return indy
 }
 
 func prepareCacheDirectories() {
@@ -183,21 +186,6 @@ func decorateChecksums(downloads []string) []string {
 	}
 	return finalDownloads
 }
-
-// func replaceTargets(artifacts []string, oldIndyHost, targetIndyHost, buildName string) []string {
-// 	results := []string{}
-// 	for _, a := range artifacts {
-// 		final := replaceTarget(a, oldIndyHost, targetIndyHost, buildName)
-// 		results = append(results, final)
-// 	}
-// 	return results
-// }
-
-// func replaceTarget(artifact, oldIndyHost, targetIndyHost, buildName string) string {
-// 	final := replaceHost(artifact, oldIndyHost, targetIndyHost)
-// 	final = replaceBuildName(final, buildName)
-// 	return final
-// }
 
 func replaceHost(artifact, oldIndyHost, targetIndyHost string) string {
 	// First, replace the embedded indy host to the target one
@@ -238,7 +226,7 @@ func generateRandomBuildName() string {
 	return fmt.Sprintf(buildPrefix+"%v", rand.Intn(max-min)+min)
 }
 
-func concurrentRun(numWorkers int, artifacts []string, job func(artifact string) bool) bool {
+func concurrentRunForDownload(numWorkers int, artifacts []string, job func(artifact string) bool) bool {
 	ch := make(chan string, numWorkers*5) // This buffered number of chan can be anything as long as it's larger than numWorkers
 	var wg sync.WaitGroup
 	var mu sync.Mutex
@@ -256,6 +244,47 @@ func concurrentRun(numWorkers int, artifacts []string, job func(artifact string)
 				}
 				mu.Lock()
 				results = append(results, job(a))
+				mu.Unlock()
+			}
+		}()
+	}
+
+	// Now the jobs can be added to the channel, which is used as a queue
+	for _, artifact := range artifacts {
+		ch <- artifact // add artifact to the queue
+	}
+
+	close(ch) // This tells the goroutines there's nothing else to do
+	wg.Wait() // Wait for the threads to finish
+
+	finalResult := true
+	for _, result := range results {
+		if finalResult = result; !finalResult {
+			break
+		}
+	}
+
+	return finalResult
+}
+
+func concurrentRunForUpload(numWorkers int, artifacts map[string][]string, job func(originalArtiURL, targetArtiURL string) bool) bool {
+	ch := make(chan []string, numWorkers*5) // This buffered number of chan can be anything as long as it's larger than numWorkers
+	var wg sync.WaitGroup
+	var mu sync.Mutex
+	var results = []bool{}
+
+	// This starts numWorkers number of goroutines that wait for something to do
+	wg.Add(numWorkers)
+	for i := 0; i < numWorkers; i++ {
+		go func() {
+			for {
+				a, ok := <-ch
+				if !ok { // if there is nothing to do and the channel has been closed then end the goroutine
+					wg.Done()
+					return
+				}
+				mu.Lock()
+				results = append(results, job(a[0], a[1]))
 				mu.Unlock()
 			}
 		}()
