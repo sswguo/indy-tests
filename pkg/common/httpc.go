b48bf1f073fe12da7d6be5df6a332bbdf3d53081
Rui Han <ruhan@redhat.com>
2021-12-31 16:12:52 +0800

    Debug to print resp.Status (#90)

--- pkg/common/httpc.go
+++ pkg/common/httpc.go
@@ -205,7 +205,7 @@ func HTTPRequest(url, method string, auth Authenticate, needResult bool, dataPay
 	}
 	resp, err := client.Do(req)
 	if err != nil {
-		fmt.Printf("Client failed, %s\n", err)
+		fmt.Printf("Client failed, Error: %s, Status: %s\n", err, resp.Status)
 		return respText, StatusUnknown, false
 	}
 
